3fcb22f9810a5c585e99f43d829dc86d2eb9367e
Lance <lpery@academic.rrc.ca>
2025-03-05 22:46:25 -0600

    Added Pokemon, Moves and Items Controllers, and added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  root 'pokemon#index'
+
+  resources :pokemon, only: [:index, :show]
+  resources :moves, only: [:index, :show]
+  resources :items, only: [:index]
+  # get "items/index"
+  # get "items/show"
+  # get "moves/index"
+  # get "moves/show"
+  # get "pokemon/index"
+  # get "pokemon/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
