ffbc1d63d23ab8cbe9ebafd8947453583610e5b0
Lance <lpery@academic.rrc.ca>
2025-03-06 20:30:18 -0600

    Added the search for the items

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,12 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.paginate(page: params[:page], per_page: 10)
+    @items = Item.all
+
+    if params[:search].present?
+      @items = @items.where("LOWER(name) LIKE ?", "%#{params[:search].downcase}%")
+    end
+
+    @items = @items.paginate(page: params[:page], per_page: 10)
   end
 
   def show
