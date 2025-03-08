fdf96182505a4d3ce5529484e0011073b79b67b2
Lance <lpery@academic.rrc.ca>
2025-03-08 00:15:10 -0600

    Added the pokemon and moves show link to the types

--- app/models/move.rb
+++ app/models/move.rb
@@ -1,10 +1,11 @@
 class Move < ApplicationRecord
   has_many :moves_types
-  has_many :types, through: :moves_types
+  has_many :type, through: :moves_types
+  belongs_to :type
   has_and_belongs_to_many :pokemons
 
   validates :name, presence: true, uniqueness: true
-  validates :accuracy, :power, :pp, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :accuracy, :power, :pp, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates :category, presence: true
   validates :move_type, presence: true
 end
