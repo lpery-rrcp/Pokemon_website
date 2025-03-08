c0ea2881a37b5897faf60fa1a854dd90a3ff9862
Lance <lpery@academic.rrc.ca>
2025-03-08 00:53:22 -0600

    Added the ERB conditional statment for pokemon types

--- app/models/type.rb
+++ app/models/type.rb
@@ -5,4 +5,30 @@ class Type < ApplicationRecord
 
 
   validates :name, presence: true, uniqueness: true
+
+   # Method to find weaknesses based on type
+   def weaknesses
+    type_weaknesses = {
+      "Fire" => [ "Water", "Rock", "Ground" ],
+      "Water" => [ "Electric", "Grass" ],
+      "Electric" => [ "Ground" ],
+      "Grass" => [ "Fire", "Ice", "Poison", "Flying", "Bug" ],
+      "Ice" => [ "Fire", "Fighting", "Rock", "Steel" ],
+      "Fighting" => [ "Flying", "Psychic", "Fairy" ],
+      "Psychic" => [ "Bug", "Ghost", "Dark" ],
+      "Bug" => [ "Fire", "Flying", "Rock" ],
+      "Ghost" => [ "Ghost", "Dark" ],
+      "Dark" => [ "Fighting", "Bug", "Fairy" ],
+      "Fairy" => [ "Fire", "Poison", "Steel" ],
+      "Poison" => [ "Ground", "Psychic" ],
+      "Flying" => [ "Electric", "Ice", "Rock" ],
+      "Rock" => [ "Water", "Grass", "Fighting", "Ground", "Steel" ],
+      "Ground" => [ "Water", "Ice", "Grass", "Flying" ],
+      "Steel" => [ "Fire", "Fighting", "Ground" ],
+      "Dragon" => [ "Ice", "Dragon", "Fairy" ],
+      "Normal" => [ "Fighting" ]
+    }
+
+    type_weaknesses[self.name] || []  # Return weaknesses for this type
+  end
 end
