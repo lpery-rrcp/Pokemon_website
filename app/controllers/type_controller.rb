646510a5672a03e07c669aaf7909e4f108e55646
Lance <lpery@academic.rrc.ca>
2025-03-07 20:07:55 -0600

    Added the pokemonType join table

--- app/controllers/type_controller.rb
+++ app/controllers/type_controller.rb
@@ -1,10 +1,5 @@
-<<<<<<< HEAD
-class TypeController < ApplicationController
-  def name
-=======
 class TypesController < ApplicationController
   def name
     @types = Type.all
->>>>>>> recovery-branch
   end
 end
--- app/models/type.rb
+++ app/models/type.rb
@@ -1,7 +1,8 @@
 class Type < ApplicationRecord
-  has_and_belongs_to_many :pokemons
+  has_and_belongs_to_many :pokemons, join_table: "pokemon_types"
   has_many :moves_types
   has_many :moves, through: :moves_types
+
 
   validates :name, presence: true, uniqueness: true
 end
